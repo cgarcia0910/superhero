babf2d8d9aca8cb2f99243b670e59c87817d432e
Carlos Garcia <cgarcia0910@gmail.com>
2024-05-06 17:21:25 +0200

    TASK: Manage events of create delete update

--- src/app/pages/add-superhero/add-superhero.component.ts
+++ src/app/pages/add-superhero/add-superhero.component.ts
@@ -1,24 +1,52 @@
-import { Component, inject } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
 import { AddEditSuperheroComponent } from '../../features/add-edit-superhero/add-edit-superhero.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { MODEL_SUPERHERO_DISPLAYER } from '../../adapter/domain/ports/i-model-displayer';
 import { Superhero } from '../../adapter/domain/models/superhero';
 import { firstValueFrom } from 'rxjs';
+import { CgcConfirmationModalComponent } from '../../shared/components/cgc-confirmation-modal/cgc-confirmation-modal.component';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-add-superhero',
   standalone: true,
-  imports: [AddEditSuperheroComponent, MatToolbarModule],
+  imports: [AddEditSuperheroComponent, MatToolbarModule, MatProgressSpinnerModule],
   templateUrl: './add-superhero.component.html',
   styleUrl: './add-superhero.component.scss'
 })
 export class AddSuperheroComponent {
-  _modelSuperHeroDisplayer = inject(MODEL_SUPERHERO_DISPLAYER)
+  _modelSuperHeroDisplayer = inject(MODEL_SUPERHERO_DISPLAYER);
+  _dialog: MatDialog = inject(MatDialog);
+  _destroyRef: DestroyRef = inject(DestroyRef);
+
+  loading = signal<boolean>(false);
+  
+  private _router: Router = inject(Router);
   doAction(event: {action: string, value: unknown}) {
     const {action = undefined, value = undefined} = event || {};
     switch(action) {
       case 'save-superhero':
-        firstValueFrom(this._modelSuperHeroDisplayer.addSuperhero(value as Superhero)).then(e => console.log(e))
+        this.loading.set(true);
+        firstValueFrom(this._modelSuperHeroDisplayer.addSuperhero(value as Superhero)).then(e => {
+          this.loading.set(false);
+          this._dialog.open(CgcConfirmationModalComponent, {
+            data: {
+              title: "Create superhero",
+              message: "The superhero has been created sucessfully",
+              buttons: [
+                {label: 'Accept', value: 'accept'}
+              ]
+            }
+          }).afterClosed().pipe(takeUntilDestroyed(this._destroyRef)).subscribe(modalResponse => {
+            switch(modalResponse) {
+              case 'accept':
+                this._router.navigate([`/`]);
+            }
+          });
+        });
         break;
     }
   }
--- src/app/pages/filter-hero/filter-hero.component.ts
+++ src/app/pages/filter-hero/filter-hero.component.ts
@@ -5,7 +5,6 @@ import { SearchSuperheroComponent } from '../../features/search-superhero/search
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { Superhero } from '../../adapter/domain/models/superhero';
 import { Router } from '@angular/router';
-import { MatDialog} from '@angular/material/dialog';
 import { CgcConfirmationModalComponent } from '../../shared/components/cgc-confirmation-modal/cgc-confirmation-modal.component';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
 import { firstValueFrom } from 'rxjs';
@@ -20,8 +19,6 @@ import { firstValueFrom } from 'rxjs';
 export class FilterHeroComponent {
   _modelSuperHeroDisplayer = inject(MODEL_SUPERHERO_DISPLAYER);
   _router: Router = inject(Router);
-  _dialog: MatDialog = inject(MatDialog);
-  _destroyRef: DestroyRef = inject(DestroyRef);
 
   SUPERHERO_CONFIG_TABLE = SUPERHERO_CONFIG_TABLE;
   doAction(event: {action: string, value: unknown}) {
@@ -30,24 +27,6 @@ export class FilterHeroComponent {
       case 'edit-superhero':
         const {id = undefined} = value as Superhero || {};
         this._router.navigate([`edit-superhero/${id}`]);
-      break
-      case 'delete-superhero':
-        this._dialog.open(CgcConfirmationModalComponent, {
-          data: {
-            title: "Delete superhero",
-            message: "Are you sure?",
-            buttons: [
-              {label: 'Cancel', value: 'cancel'},
-              {label: 'Delete', value: 'delete'}
-            ]
-          }
-        }).afterClosed().pipe(takeUntilDestroyed(this._destroyRef)).subscribe(modalResponse => {
-          switch(modalResponse) {
-            case 'delete':
-              firstValueFrom(this._modelSuperHeroDisplayer.removeSuperhero(value as Superhero)).then(e => console.log(e))
-            break;
-          }
-        });
       break;
     }
   }
